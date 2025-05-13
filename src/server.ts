ec680c2f4bab55c1d9c160fd606fda3efc55aec2
Fidel <fidel050987@gmail.com>
2025-05-13 10:36:09 -0600

    arreglando conexión a bd de producción

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,8 @@ async function connectDB() {
         db.sync()
         console.log(colors.blue.bold('Conexión exitosa a BD'))
     } catch (error) {
-        //console.log(error)
+        console.log(error)
         console.log(colors.red.bold('Falló la Conexión a la BD'))
-
     }
 }
 
