8d6a2ad8fa012fc0d30576299dfb7af96526b3d7
Fidel <fidel050987@gmail.com>
2025-05-13 11:56:39 -0600

    quitando ssl de la conexión a la bd

--- src/config/db.ts
+++ src/config/db.ts
@@ -11,5 +11,8 @@ export const db = new Sequelize({
     host: process.env.DB_HOST,
     dialect: "mysql",
     port: Number(process.env.DB_PORT) || 3306, // Si no hay puerto en .env, usa 3306
-    logging: false
+    logging: false,
+    dialectOptions: {
+        ssl: false
+    }
 });
